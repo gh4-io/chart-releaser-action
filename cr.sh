2670018a6ef0ff9cd7e9ff7e7e55399a21eda8b3
gh4-io <123490066+gh4-io@users.noreply.github.com>
2023-01-30 22:20:59 -0500

    Update cr.sh
    Signed-off-by: gh4-io <123490066+gh4-io@users.noreply.github.com>

--- cr.sh
+++ cr.sh
@@ -237,7 +237,7 @@ filter_charts() {
     while read -r chart; do
         if [[ -f "$chart" ]]; then
             [[ ! "$chart" == "Chart.yaml" ]] && continue
-            echo "."
+            echo "./"
         elif [[ -d "$chart" ]]; then
             local file="$chart/Chart.yaml"
             if [[ -f "$file" ]]; then
